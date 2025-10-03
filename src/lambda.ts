338ca28aa0da2d0f305886dffc1cd07eb916fcf8
Max <maximiliano.vega.c@gmail.com>
2025-10-02 18:46:11 -0700

    No more warnings

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,28 +1,45 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import { Handler, Context, Callback } from 'aws-lambda';
+import {
+  Handler,
+  Context,
+  Callback,
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+} from 'aws-lambda';
 import express from 'express';
 import serverless from 'serverless-http';
 
-let cachedServer: any;
+let cachedServer: serverless.Handler | undefined;
 
-async function createNestServer(): Promise<any> {
+async function createNestServer(): Promise<serverless.Handler> {
   if (!cachedServer) {
     const expressApp = express();
     const adapter = new ExpressAdapter(expressApp);
     const app = await NestFactory.create(AppModule, adapter);
-    
+
     // Enable CORS if needed
     app.enableCors();
-    
+
     await app.init();
     cachedServer = serverless(expressApp);
   }
   return cachedServer;
 }
 
-export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
+export const handler: Handler<
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult
+> = async (
+  event: APIGatewayProxyEvent,
+  context: Context,
+  callback: Callback,
+) => {
   const server = await createNestServer();
-  return server(event, context, callback);
+  return server(
+    event as Parameters<serverless.Handler>[0],
+    context as Parameters<serverless.Handler>[1],
+    callback as Parameters<serverless.Handler>[2],
+  );
 };
